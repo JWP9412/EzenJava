d512ea9f6f323ea091aa3d11ff9ccd20c8f7a75d
JWP <jiwon9412@gmail.com>
2021-05-21 17:42:30 +0900

    [Lambda] finish

---
+++ Fx/src/module-info.java
@@ -0,0 +1,5 @@
+module Fx {
+	requires javafx.controls;
+	
+	opens application to javafx.graphics, javafx.fxml;
+}
---
+++ Lambda/src/exam06/LambdaExample06.java
@@ -0,0 +1,51 @@
+package exam06;
+
+import java.util.function.ToIntFunction;
+
+public class LambdaExample06 {
+	private static Student[] students = { 
+			new Student("Hong", 90, 96), 
+			new Student("Shin", 95, 93) };
+
+	// avg() 메소드 작성
+	public static double avg(ToIntFunction<Student> tif) {
+		int sum = 0;
+		for (Student student : students) {
+			sum += tif.applyAsInt(student);
+		}
+		return (double) sum/students.length;
+	}
+
+	public static void main(String[] args) {
+		double engAvg = avg(s -> s.getEngScore());
+		System.out.println("Eng Avg : " + engAvg);
+
+		double mathAvg = avg(s -> s.getMathScore());
+		System.out.println("Math Avg : " + mathAvg);
+
+	}
+
+	public static class Student {
+		private String name;
+		private int engScore;
+		private int mathScore;
+
+		public Student(String name, int engScore, int mathScore) {
+			this.name = name;
+			this.engScore = engScore;
+			this.mathScore = mathScore;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public int getEngScore() {
+			return engScore;
+		}
+
+		public int getMathScore() {
+			return mathScore;
+		}
+	}
+}
